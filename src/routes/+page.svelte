bc14a0133857a515ed2c5f8e4c2539b2aead333f
Egor Demeshko <demeshko.egor@gmail.com>
2024-03-29 20:28:02 +0300

    staged

---
+++ src/routes/+page.svelte
@@ -0,0 +1,117 @@
+<script>
+    let blurEffect = 20;
+</script>
+
+<video class="background" src="/bubbles.mp4" loop autoplay muted playsinline>
+</video>
+<div class="blur" style="backdrop-filter: blur({blurEffect}px);">
+
+</div>
+
+<div class="header">
+    <p>СТЕКЛО</p>
+    <p>НЕСТЕКЛО</p>
+</div>
+
+<div class="fishka">
+    <h1>Супер важная фишка</h1>
+</div>
+
+<div class="fishka2">
+    <h1>ЕЩЕ Супер важная фишка</h1>
+</div>
+
+<div class="range">
+    <input  type="range" bind:value={blurEffect}/>
+</div>
+
+<style>
+    :global(body){
+        margin: 0;
+    }
+    * *{
+        box-sizing: border-box;
+    }
+
+    .background {
+        width: 100vw;
+        height: 100vh;
+        box-sizing: border-box;
+        object-fit: cover;
+    }
+
+    .blur{
+        background-color: #040A0859;
+        width: 50vw;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        left: 0;
+    }
+
+    .header{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        backdrop-filter: blur(30px);
+        box-shadow: 0 0 6px black;
+    }
+
+    p{
+        color: white;
+        font-size: 24px;
+        font-weight: bold;
+
+    }
+
+
+    h1{
+        font-size: 48px;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+
+    .fishka{
+        height: 250px;
+        width: 484px;
+        background-color: #75747466;
+        position: absolute;
+        left: 50%;
+        top: 20%;
+        transform: translate(-50%);
+        border-radius: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        color: beige;
+}
+
+    .fishka2{
+        height: 250px;
+        width: 484px;
+        background-color: #323232BF;
+        position: absolute;
+        left: 50%;
+        top: 60%;
+        transform: translate(-50%);
+        border-radius: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        color: beige;
+    }
+
+    .range{
+        position: absolute;
+        left: 10%;
+        top: 70%;
+        display: block;
+        z-index: 10;
+    }
+</style>
